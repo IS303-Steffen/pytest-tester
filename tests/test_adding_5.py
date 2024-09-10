1c85c7f8c69be4141021e6114549e03360fd0ba3
jacobsteffenBYU <jacob.steffen@byu.edu>
2024-09-10 14:52:29 -0600

    conftest tests

--- tests/test_adding_5.py
+++ tests/test_adding_5.py
@@ -1,7 +1,7 @@
 from example_function import add_5
 
 
-def test_add_five():
+def test_add_five(sample_number):
     assert add_5(3) == 8
-    assert add_5(10) == 15
+    assert add_5(sample_number) == 15
     assert add_5(-5) == 0
