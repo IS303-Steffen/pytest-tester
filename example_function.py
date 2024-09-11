75e368a40feaf86763166624c065778af5d04c37
jacobsteffenBYU <jacob.steffen@byu.edu>
2024-09-11 10:24:40 -0600

    updating script

--- example_function.py
+++ example_function.py
@@ -1,6 +1,7 @@
 def add_5(num):
     return num +5
 
-example_input = input("Give me an example input: ")
-
-example_input_2 = input("Give me an example input 2: ")
+# student_script.py
+print("This is the first output.")
+print("Here is the second output!")
+print("Finally, the third output.")
